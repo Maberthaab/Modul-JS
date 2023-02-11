4153107317a16acff4ab44a69fa00654a4482a39
Maberthaab <thataaberlian@gmail.com>
2023-02-12 00:50:52 +0700

    first commit

---
+++ Onload.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Body onload example</title>
+</head>
+<body onload="alert('Halaman ini telah selesai di loading')">
+welcome to my page
+
+</body>
+</html>
---
+++ onmouseover dan out.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<table>
+<tr onmouseover="this.bgColor='lightblue'" 
+onmouseout="this.bgColor='#efefef'" bgColor="#efefef">
+<td>Baris Pertama</td>
+</tr>
+<tr>
+<td>Baris Kedua</td>
+</tr>
+</body>
+</html>
